22e139a51638e7ed63395b62c948c40324507735
Marius Vorster <marius.vrstr@gmail.com>
2012-10-03 16:17:02 +0200

    JVO > Added Unit Test project to solution

---
+++ MVVMExample.Tests/StudentTest.cs
@@ -0,0 +1,45 @@
+﻿using MVVMExample.ViewModels;
+using MVVMExample.ViewModels.DomainModels;
+using NUnit.Framework;
+
+namespace MVVMExample.Tests
+{
+    [TestFixture(18)]
+    public class TestStudent
+    {
+        private int MinAllowedAge { get; set; }
+
+        public TestStudent(int minAllwed)
+        {
+            MinAllowedAge = minAllwed;
+        }
+
+        #region Test if save is allowed
+
+            [TestCase(18, "Marius", "Vorster")]  // Test A
+            [TestCase(22, null, "Vorster")]      // Test B
+            [TestCase(12, "Marius", "Vorster")]  // Test C
+            public void TestCanSave(int age, string name, string surname)
+            {
+                var vm = new StudentData();
+                // Build test object
+                vm.Student = new Student
+                                 {
+                                     FirstName = name,
+                                     LastName = surname,
+                                     Age = age
+                                 };
+
+                bool result = vm.CanSaveStudent(new object());
+
+                // This pre-validates the object to determine the expected result is
+                bool expectedResult = (((vm.Student.Age >= MinAllowedAge)) &&
+                    (!(string.IsNullOrEmpty(vm.Student.FirstName))) &&
+                    (!(string.IsNullOrEmpty(vm.Student.LastName)))); 
+
+                Assert.AreEqual(expectedResult, result);
+            }
+
+        #endregion
+    }
+}
